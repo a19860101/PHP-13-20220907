5b325d9071f9557d474dde3af1f12f296ccd3190
Zac <a19860101@gmail.com>
2022-09-12 19:06:49 +0800

    20220912

---
+++ condition.php
@@ -0,0 +1,29 @@
+<?php
+    $x = -19;
+    // if...
+    if($x > 0){
+        echo 'Success';
+    }
+
+    // if...else
+    if($x > 0){
+        echo 'Success 2';
+    }else{
+        echo 'Error 2';
+    }
+
+    // if...elseif
+    if($x > 0){
+        echo '大於0';
+    }elseif($x < 0){
+        echo '小於0';
+    }
+
+    //if...elseif...else
+    if($x > 0){
+        echo '大於0';
+    }elseif($x < 0){
+        echo '小於0';
+    }else{
+        echo 'Error 3';
+    }

c5e33dccd3d4d38edce52b374a21251fc5f86963
Zac <a19860101@gmail.com>
2022-09-26 19:44:59 +0800

    20220926

--- class/basic.php
+++ class/basic.php
@@ -2,18 +2,25 @@
     class Test {
         public $skin = 'white';
         public $height;
+        public $name = 'undefined';
+
+        public function go(){
+            return $this->name.' gogogo';
+        } 
     }
 
     $john = new Test;
 
     // print_r($john);
 
-    echo $john->skin;
-    echo $john->height;
+    // echo $john->skin;
+    // echo $john->height;
 
 
     $mary = new Test;
+    $mary->name = 'Mary';
     $mary->skin = 'black';
     $mary->height = 177;
-    echo $mary->skin;
-    echo $mary->height;
+    echo $mary->go();
+    // echo $mary->skin;
+    // echo $mary->height;

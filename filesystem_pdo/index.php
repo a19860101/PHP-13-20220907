f9e8d2689f0d972e0f71c691a1040d11e2d8821d
Zac <a19860101@gmail.com>
2022-09-26 19:22:24 +0800

    20220926

--- filesystem_pdo/index.php
+++ filesystem_pdo/index.php
@@ -4,7 +4,13 @@
     $imgs = pdo()->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     $img_num = count($imgs);
     if(isset($_REQUEST['delete'])){
-        unlink($_REQUEST['img']);
+        extract($_REQUEST);
+        unlink('images/'.$path);
+        $sql = 'DELETE FROM galleries WHERE id = ?';
+        $stmt = pdo()->prepare($sql);
+        $stmt->execute([$id]);
+        echo '<script>alert("圖片已刪除");</script>';
+        header('refresh:0;url=index.php');
     }
 ?>
 <!DOCTYPE html>
@@ -32,7 +38,8 @@
         <?php foreach($imgs as $img){ ?>
             <img src="images/<?php echo $img['path']; ?>" width= "100">
             <form action="" method="post">
-                <input type="hidden" name="img" value="<?php echo $img;?>">
+                <input type="hidden" name="id" value="<?php echo $img['id'];?>">
+                <input type="hidden" name="path" value="<?php echo $img['path'];?>">
                 <input type="submit" value="刪除" name="delete" onclick="return confirm('確認刪除？')">
             </form>
         <?php } ?>

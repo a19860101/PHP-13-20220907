e30710d7423867db1f9edf58b7844c69f3bc9b6a
Zac <a19860101@gmail.com>
2022-09-26 20:36:13 +0800

    20220926

--- class/basic.php
+++ class/basic.php
@@ -4,6 +4,10 @@
         public $hp = 100;
         private $role = 'Human';
         protected $magic = 'FLY';
+
+        function __construct(){
+            echo '建構子';
+        }
         static function walk(){
 
             return 'walk';
@@ -26,11 +30,14 @@
 
     echo User::walk();
 
-    // $max = new User;
-    // echo $max->walk();
+    $max = new User;
+    echo $max->walk();
     // $max->name = 'Max';
     // echo $max->magic;
     // echo $max->test();
-    // $npc1 = new NPC;
-    // echo $npc1 -> test2();
+    $npc1 = new NPC;
+    echo $npc1 -> test2();
+
+    $terry = new User;
+    $npc2 = new NPC;
   

d54d8bc46f0794f1674fd1c85663df16498923d8
Zac <a19860101@gmail.com>
2022-10-03 19:33:58 +0800

    20221003

---
+++ blog/public/post/create.php
@@ -0,0 +1,33 @@
+<?php include('../template/header.php'); ?>
+<?php include('../template/nav.php'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2>新增文章</h2>
+        </div>
+        <div class="col-6">
+            <form action="store.php" method="post">
+                <div class="mb-3">
+                    <label for="" class="form-label">文章標題</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">文章分類</label>
+                    <select name="category_id" id="" class="form-select">
+                        <option>--請選擇--</option>
+                        <option value="1">科技</option>
+                        <option value="2">美食</option>
+                        <option value="3">生活</option>
+                        <option value="4">運動</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">文章內容</label>
+                    <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <input type="submit" class="btn btn-primary" value="建立文章">
+            </form>
+        </div>
+    </div>
+</div>
+<?php include('../template/footer.php'); ?>
--- blog/public/post/index.php
+++ blog/public/post/index.php
@@ -4,6 +4,7 @@
     <div class="row">
         <div class="col-12">
             <h2>文章列表</h2>
+            <a href="create.php" class="btn btn-primary">新增文章</a>
         </div>
     </div>
 </div>

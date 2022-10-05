7d051384dbf8c8e1ace4aee4b2ed1dcf8d4f7ac1
Zac <a19860101@gmail.com>
2022-10-05 20:33:40 +0800

    20221005

--- blog/public/post/show.php
+++ blog/public/post/show.php
@@ -17,11 +17,16 @@
                 <div class="text-end text-muted">發布時間:<?php echo $post['created_at']; ?></div>
                 <div class="text-end text-muted">最後更新時間:<?php echo $post['updated_at']; ?></div>
                 <a href="index.php" class="btn btn-success btn-sm">文章列表</a>
+
+                <?php if(isset($_SESSION['AUTH']) && $_SESSION['AUTH']['id'] == $post['user_id']){ ?>
                 <a href="edit.php?id=<?php echo $post['id'];?>" class="btn btn-info btn-sm">編輯文章</a>
                 <form action="delete.php" method="post" class="d-inline-block">
                     <input type="hidden" name="id" value="<?php echo $post['id']; ?>">
                     <input type="submit" value="刪除文章" class="btn btn-danger btn-sm" onclick="return confirm('確認刪除?');">
                 </form>
+                <?php } ?>
+
+
             </div>
         </div>
     </div>

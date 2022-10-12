9e8cc4e9a30c2605d01fa97a629314820e0ea78b
Zac <a19860101@gmail.com>
2022-10-12 21:37:39 +0800

    20221012

--- laravel/app/Http/Controllers/ProductController.php
+++ laravel/app/Http/Controllers/ProductController.php
@@ -16,14 +16,23 @@ class ProductController extends Controller
         return view('admin.product.create');
     }
     public function store(Request $request){
-        DB::insert('INSERT INTO products(title,description,price,special_price,publish_at,unpublish_at,published)VALUES(?,?,?,?,?,?,?)',[
-            $request->title,
-            $request->description,
-            $request->price,
-            $request->special_price,
-            $request->publish_at,
-            $request->unpublish_at,
-            $request->published
+        // DB::insert('INSERT INTO products(title,description,price,special_price,publish_at,unpublish_at,published)VALUES(?,?,?,?,?,?,?)',[
+        //     $request->title,
+        //     $request->description,
+        //     $request->price,
+        //     $request->special_price,
+        //     $request->publish_at,
+        //     $request->unpublish_at,
+        //     $request->published
+        // ]);
+        DB::table('products')->insert([
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'special_price' => $request->special_price,
+            'publish_at'    => $request->publish_at,
+            'unpublish_at'  => $request->unpublish_at,
+            'published'     => $request->published
         ]);
         return redirect('/admin/product');
     }

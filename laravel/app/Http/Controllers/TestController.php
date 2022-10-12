542e148a2b4cafd45fcd4cca7e2ab137614d8a1d
Zac <a19860101@gmail.com>
2022-10-12 19:29:44 +0800

    20221012

--- laravel/app/Http/Controllers/TestController.php
+++ laravel/app/Http/Controllers/TestController.php
@@ -22,6 +22,6 @@ class TestController extends Controller
         return view('form');
     }
     public function res(Request $request){
-        return $request;
+        return $request->email;
     }
 }

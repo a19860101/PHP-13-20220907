e510c97dc6da5bc73948912101e50a743729cf30
Zac <a19860101@gmail.com>
2022-10-26 21:20:01 +0800

    20221026

--- laravel/app/Models/Product.php
+++ laravel/app/Models/Product.php
@@ -22,4 +22,7 @@ class Product extends Model
     public function carts(){
         return $this->hasMany(Cart::class);
     }
+    public function orderDetails(){
+        return $this->hasMany(OrderDetail::class);
+    }
 }
